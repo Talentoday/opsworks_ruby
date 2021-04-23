cd6b28b5c4cc752f0b6343d48398695855ca3394
Alexandre Chakroun <alexandre.chakroun@gmail.com>
2021-04-23 12:16:27 +0200

    [Override] Resque-Scheduler: add

--- libraries/drivers_worker_resque.rb
+++ libraries/drivers_worker_resque.rb
@@ -10,12 +10,40 @@ module Drivers
 
       def configure
         add_worker_monit
+        add_scheduler_monit if configure_resque_web
       end
 
       def after_deploy
         restart_monit
+
+        restart_scheduler_monit if configure_resque_web
       end
       alias after_undeploy after_deploy
+
+      private
+
+      def add_scheduler_monit
+        opts = { application: app['shortname'], name: app['name'], out: out, deploy_to: deploy_dir(app),
+                 environment: environment, adapter: adapter, app_shortname: app['shortname'] }
+
+        context.template File.join(node['monit']['basedir'], "resque-scheduler_#{opts[:application]}.monitrc") do
+          mode '0640'
+          source "resque-scheduler.monitrc.erb"
+          variables opts
+        end
+      end
+
+      def restart_scheduler_monit
+        return if ENV['TEST_KITCHEN'] # Don't like it, but we can't run multiple processes in Docker on travis
+
+        context.execute "monit restart resque_#{app['shortname']}-scheduler" do
+          retries 3
+        end
+      end
+
+      def configure_resque_web
+        node['deploy'][app['shortname']].try(:[], 'worker').try(:[], 'enable_resque_scheduler')
+      end
     end
   end
 end
